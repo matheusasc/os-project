563624fe9f9cb11693388781983e65fd0eacdc39
matheusasc <matheus.assuncao994@gmail.com>
2025-03-31 00:24:56 -0400

    feat - inserido header no app.component juntamente dos metodos de logout e criar nova os

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,17 +1,40 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 import { AsyncPipe } from '@angular/common';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AsyncPipe],
+  imports: [CommonModule, RouterOutlet, AsyncPipe, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   private authService = inject(AuthService);
   isAuthenticated$ = this.authService.isAuthenticated$;
+  isRouterActive = true;
+
+  constructor(
+    private router: Router) { }
+
+  onLogout(): void {
+    this.authService.logout();
+  }
+
+  createNewOrder(): void {
+    this.isRouterActive = false;
+    setTimeout(() => {
+      this.isRouterActive = true;
+      this.router.navigateByUrl('/os/new');
+    }, 50);
+  }
+
+  private resetRouterOutlet(): void {
+    this.isRouterActive = false;
+    setTimeout(() => this.isRouterActive = true);
+  }
+
 }
